7fb82e50a29f5ba6b3670ed3ef675ee5a85a117d
Marcoseleck <marcoseleck@gmail.com>
2015-05-06 11:45:12 -0700

    IMPLEMANTACION DE LA OPCION DE ORDENACION Y ENVIO DE ARREGLO

--- app/src/main/java/com/movil/desarrollo/tareasensegundoplano/Hilos.java
+++ app/src/main/java/com/movil/desarrollo/tareasensegundoplano/Hilos.java
@@ -3,33 +3,40 @@ package com.movil.desarrollo.tareasensegundoplano;
 
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.widget.Toast;
 
 /**
  * Created by Marcos on 5/3/2015.
  */
 public class Hilos {
-
-    Handler handle = new Handler();
-        public  void miThread(){
+int opcion;
+       //METODO CON PARAMETROS DE LA OPCION Y ARREGLO
+        public  void miThread(final int opcion, float [] datos){
+            this.opcion=opcion;
 /*SE CREA UN HILO PARA PODER DETENRLO UNOS SEGUNDOS Y SABER IDENTIFICAR CUANDO EMPIEZA EL METODO DE ORDENACION*/
             Thread t = new Thread(){
                 public void run(){
-                    try{
-                        Thread.sleep(10);
+                    /*Segun la opcion que obtengamos de la invocacion del metodo*/
+                    switch (opcion) {
 
-                    }catch(InterruptedException e){
-                        e.printStackTrace();
+                        case 1:    Log.w("Hilo","METODO1");
+                            ;
+                            break;
+
+                        case 2:Log.w("Hilo","METODO2");
+                            break;
+                        case 3:
+                            Log.w("Hilo","METODO3");
+                            break;
+
                     }
-                    /*SE LLAMA EL HILO DONDE ESTA DENTRO EL METODO DE ORDENACION */
-                    handle.post(proceso);
+                        
+
                 }
             };
             t.start();
         }
 
- Runnable proceso = new Runnable(){
-    public void run(){
-/*AQUI SE INTRODUCE EL OBJETO DE LA CLASE DE ORDENACION Y SE LLAMAN SUS METODOS*/
-    }
-};}
+
+}
--- app/src/main/java/com/movil/desarrollo/tareasensegundoplano/MainActivity.java
+++ app/src/main/java/com/movil/desarrollo/tareasensegundoplano/MainActivity.java
@@ -6,7 +6,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -16,7 +17,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
 
